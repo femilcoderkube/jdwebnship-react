6c19a5a6b62c6a4fceebf761fb8c2c68a5871231
Kishan <kishansutariya.coderkube@gmail.com>
2025-09-09 17:06:12 +0530

    changes

--- src/components/CardSlider.jsx
+++ src/components/CardSlider.jsx
@@ -33,22 +33,6 @@ const cardItems = [
   },
 ];
 
-function CardItem({ item }) {
-  return (
-    <div className="w-[334.8px] h-[491px] bg-white rounded-lg shadow-lg flex flex-col flex-shrink-0">
-      <img
-        src={item.image}
-        alt={item.name}
-        className="w-[334.8px] h-[418px] object-cover rounded-t-lg"
-      />
-      <div className="p-4 flex flex-col items-center">
-        <h3 className="text-lg font-semibold text-gray-800">{item.name}</h3>
-        <p className="text-md text-gray-600">{item.price}</p>
-      </div>
-    </div>
-  );
-}
-
 export default function CardSlider() {
   return (
     <div className="w-full max-w-7xl mx-auto py-8">
