e00cf16d87dbaa5c240d4536170f0ccacdb0bedf
Kishan <kishansutariya.coderkube@gmail.com>
2025-09-09 17:06:00 +0530

    changes

--- src/components/CardItem.jsx
+++ src/components/CardItem.jsx
@@ -1,15 +1,6 @@
-import React from "react";
-
-export default function CardItem() {
-  const item = {
-    image:
-      "https://images.unsplash.com/photo-1506744038136-46273834b3fb?auto=format&fit=crop&w=334.8&h=418&q=80",
-    name: "Mountain Retreat",
-    price: "$299.99",
-  };
-
+function CardItem({ item }) {
   return (
-    <div className="w-[334.8px] h-[491px] bg-white rounded-lg shadow-lg flex flex-col">
+    <div className="w-[334.8px] h-[491px] bg-white rounded-lg shadow-lg flex flex-col flex-shrink-0">
       <img
         src={item.image}
         alt={item.name}
