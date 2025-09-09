00698d8d0292697ce3f09be02d7b2ced2dd11481
Kishan <kishansutariya.coderkube@gmail.com>
2025-09-09 11:40:50 +0530

    changes

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { useTheme } from "../contexts/ThemeContext";
 
 function Search() {
+  const { theme, headerTextColor } = useTheme();
   return (
     <div className="relative">
       {/* Settings Button */}
@@ -15,7 +16,7 @@ function Search() {
       >
         <path
           d="M17.8141 17.8141L22.75 22.75M11.7813 20.3125C16.4929 20.3125 20.3125 16.4929 20.3125 11.7813C20.3125 7.06956 16.4929 3.25 11.7813 3.25C7.06956 3.25 3.25 7.06956 3.25 11.7813C3.25 16.4929 7.06956 20.3125 11.7813 20.3125Z"
-          stroke="#111111"
+          stroke={headerTextColor || "#111111"}
           stroke-width="1.625"
           stroke-linecap="round"
           stroke-linejoin="round"
